c8fbd17c44b092498131d25a08711682f3fceb25
Ryo SUETSUGU <suetsugu.r@gmail.com>
2015-04-25 21:44:47 +0900

    Initial Commit

---
+++ ASButtons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ASButtons
+//
+//  Created by suer on 2015/04/25.
+//  Copyright (c) 2015年 codefirst.org. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
