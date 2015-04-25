669a9cbba579073c96964a0a561bcb981fe45d85
Ryo SUETSUGU <suetsugu.r@gmail.com>
2015-04-25 22:56:07 +0900

    Removed: generated comments

---
+++ ASButtons/MainViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
